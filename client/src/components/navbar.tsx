cf132e13158d3c7d6aedfc2ad08b184272659fb2
Iyin0 <p2iyinoluwa@gmail.com>
2022-12-11 21:37:12 +0100

    mobile view: navbar and footer

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useRef } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import '../styles/navbar.scss'
 
 const Navbar = () => {
-
-    const navigate = useNavigate()
 
     enum Routes {
         HOME = '/',
@@ -15,6 +13,11 @@ const Navbar = () => {
 
     const location = useLocation()
     const navRoutes = useRef<HTMLDivElement>(null!)
+    const [toggleSideNav, setToggleSideNav] = useState(false)
+
+    useEffect(() => {
+        setToggleSideNav(false)
+    }, [location.pathname])
 
     useEffect(() => {
         Array.from(navRoutes.current.children).forEach((child) => {
@@ -47,8 +50,13 @@ const Navbar = () => {
 
     return (
         <nav>
+            <button className="hamburger" onClick={() => setToggleSideNav(!toggleSideNav)}>
+                <svg width="40" height="35" viewBox="0 0 40 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 24.5H29.0312M10 17.25H29.0312M10 10H29.0312" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+            </button>
             <div className="nav-name">
-                <button onClick={() => navigate('/')}>ARTSY.</button>
+                <Link to='/'>ARTSY.</Link>
             </div>
             <div className="nav-routes" ref={navRoutes}>
                 <div id="home">
@@ -83,6 +91,45 @@ const Navbar = () => {
                     </svg>
                 </button>
             </div>
+            {toggleSideNav && <div className="sideNav">
+                <div className="left">
+                    <div className="top">
+                        <Link to='/'>ARTSY.</Link>
+                        <button onClick={() => setToggleSideNav(false)}>
+                            <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.317668 1.85151C0.216955 1.75079 0.137065 1.63123 0.0825594 1.49964C0.0280538 1.36805 1.06119e-09 1.22702 0 1.08459C-1.06119e-09 0.942158 0.0280538 0.801122 0.0825594 0.669534C0.137065 0.537946 0.216955 0.418381 0.317668 0.317668C0.418381 0.216955 0.537946 0.137065 0.669534 0.0825594C0.801122 0.0280538 0.942158 -1.06119e-09 1.08459 0C1.22702 1.06119e-09 1.36805 0.0280538 1.49964 0.0825594C1.63123 0.137065 1.75079 0.216955 1.85151 0.317668L13 11.4683L24.1485 0.317668C24.2492 0.216955 24.3688 0.137065 24.5004 0.0825594C24.6319 0.0280538 24.773 0 24.9154 0C25.0578 0 25.1989 0.0280538 25.3305 0.0825594C25.4621 0.137065 25.5816 0.216955 25.6823 0.317668C25.783 0.418381 25.8629 0.537946 25.9174 0.669534C25.9719 0.801122 26 0.942158 26 1.08459C26 1.22702 25.9719 1.36805 25.9174 1.49964C25.8629 1.63123 25.783 1.75079 25.6823 1.85151L14.5317 13L25.6823 24.1485C25.783 24.2492 25.8629 24.3688 25.9174 24.5004C25.9719 24.6319 26 24.773 26 24.9154C26 25.0578 25.9719 25.1989 25.9174 25.3305C25.8629 25.4621 25.783 25.5816 25.6823 25.6823C25.5816 25.783 25.4621 25.8629 25.3305 25.9174C25.1989 25.9719 25.0578 26 24.9154 26C24.773 26 24.6319 25.9719 24.5004 25.9174C24.3688 25.8629 24.2492 25.783 24.1485 25.6823L13 14.5317L1.85151 25.6823C1.75079 25.783 1.63123 25.8629 1.49964 25.9174C1.36805 25.9719 1.22702 26 1.08459 26C0.942158 26 0.801122 25.9719 0.669534 25.9174C0.537946 25.8629 0.418381 25.783 0.317668 25.6823C0.216955 25.5816 0.137065 25.4621 0.0825594 25.3305C0.0280538 25.1989 0 25.0578 0 24.9154C0 24.773 0.0280538 24.6319 0.0825594 24.5004C0.137065 24.3688 0.216955 24.2492 0.317668 24.1485L11.4683 13L0.317668 1.85151Z" fill="#333333" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="routes">
+                        <Link to={Routes.HOME}>Home</Link>
+                        <Link to={Routes.MARKETPLACE}>Marketplace</Link>
+                        <Link to={Routes.AUCTIONS}>Auctions</Link>
+                        <Link to={Routes.DROP}>Drop</Link>
+                    </div>
+                    <button className="contact">
+                        <svg width="86" height="87" viewBox="0 0 86 87" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g filter="url(#filter0_d_510_155)">
+                                <circle cx="43" cy="40" r="32" fill="#3341C1" />
+                            </g>
+                            <path d="M28 55V28.9C28 28.1025 28.2842 27.4195 28.8526 26.8511C29.42 26.2837 30.1025 26 30.9 26H54.1C54.8975 26 55.5805 26.2837 56.1489 26.8511C56.7163 27.4195 57 28.1025 57 28.9V46.3C57 47.0975 56.7163 47.7805 56.1489 48.3489C55.5805 48.9163 54.8975 49.2 54.1 49.2H33.8L28 55Z" fill="white" />
+                            <defs>
+                                <filter id="filter0_d_510_155" x="0.333333" y="0.888889" width="85.3333" height="85.3333" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                    <feOffset dy="3.55556" />
+                                    <feGaussianBlur stdDeviation="5.33333" />
+                                    <feComposite in2="hardAlpha" operator="out" />
+                                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_510_155" />
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_510_155" result="shape" />
+                                </filter>
+                            </defs>
+                        </svg>
+                    </button>
+                </div>
+                <div className="right" onClick={() => setToggleSideNav(false)}></div>
+            </div>}
         </nav>
     );
 }
